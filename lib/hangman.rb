1301ffa2114d99145ab9f71fe45cf48a45971295
itsellej <daniellehallal@gmail.com>
2018-11-06 16:23:11 +0000

    stores incorrect guess in new array but fails discards letter if not present in the word test

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,30 +1,40 @@
 class Word
 
-  attr_reader :incorrect, :letters
+  attr_reader :incorrect_guesses, :correct_guesses, :user_guesses
 
   def initialize(word)
     @word = word
-    @user_guesses = []
-    @incorrect =[]
-    @letters = []
+    @user_guesses = [] # not needed
+    @incorrect_guesses = []
+    @correct_guesses = ["_"] * @word.length
   end
 
-  def guessed_letters
-
-
-    @word.split("").each do |letter|
-      if @user_guesses.include?(letter)
-        @letters.push(letter)
-      else
-        @letters.push("_")
-        @incorrect << @user_guesses.pop
-      end
-    end
-    @letters
-    # incorrect
-  end
+  # def guessed_letters
+  #   @word.split("").each do |letter|
+  #     if @user_guesses.include?(letter)
+  #       @correct_guesses.push(letter)
+  #     else
+  #       @correct_guesses.push("_")
+  #        @incorrect_guesses.push(@user_guesses.pop)
+  #     end
+  #   end
+  #   @correct_guesses
+  # end
 
   def add_guess(letter)
     @user_guesses.push(letter)
+
+    if @word.include?(letter)
+      @word.split("").each_with_index do |character, position|
+        if letter == character
+          @correct_guesses[position] = letter
+        end
+      end
+    else
+      @correct_guesses.push("_")
+      @incorrect_guesses.push(letter)
+    end
+    @correct_guesses
   end
+
 end
--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -3,26 +3,26 @@ require 'hangman'
 RSpec.describe Word do
   it "returns the guessed letters" do
     word = Word.new('hello')
-    guessed_letters = word.guessed_letters
-    expect(guessed_letters).to eq(['_'] * 5)
+    # guessed_letters = word.guessed_letters
+    expect(word.correct_guesses).to eq(["_"] * 5)
   end
   it "returns empty letters for different lengths of words" do
     word = Word.new('blackberry')
-    guessed_letters = word.guessed_letters
-    expect(guessed_letters).to eq(['_'] * 10)
+    # guessed_letters = word.guessed_letters
+    expect(word.correct_guesses).to eq(['_'] * 10)
   end
 
   it "adds a letter to to the current word" do
     word = Word.new('hello')
     word.add_guess('h')
-    expect(word.guessed_letters).to eq(['h', '_', '_', '_', '_'])
+    expect(word.correct_guesses).to eq(['h', '_', '_', '_', '_'])
   end
 
   it "adds another letter to the current word" do
     word = Word.new('hello')
     word.add_guess('h')
     word.add_guess('e')
-    expect(word.guessed_letters).to eq(['h', 'e', '_', '_', '_'])
+    expect(word.correct_guesses).to eq(['h', 'e', '_', '_', '_'])
   end
 
   it "discards letter if not present in the word" do
@@ -31,7 +31,7 @@ RSpec.describe Word do
     word.add_guess('e')
     word.add_guess('l')
     word.add_guess('m')
-    expect(word.guessed_letters).to eq(['h', 'e', 'l', 'l', '_'])
+    expect(word.correct_guesses).to eq(['h', 'e', 'l', 'l', '_'])
   end
 
   it "stores incorrect guess in new array" do
@@ -40,7 +40,7 @@ RSpec.describe Word do
     word.add_guess('e')
     word.add_guess('l')
     word.add_guess('m')
-    expect(word.incorrect).to eq(['m'])
+    expect(word.incorrect_guesses).to eq(['m'])
   end
 
 
