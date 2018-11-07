fad0bf587c1097aeae7105fb72edceef18a35008
Elle H <itsellej@gmail.com>
2018-11-07 16:03:26 +0000

    Create Game Class. It will instantiate an instance of Word and return false when calling the solved? method

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,12 @@
 class Game
   require 'display'
+  require 'word'
 
 
-  attr_reader :display
-  
+  attr_reader :display, :word
+
   def initialize
     @display = Display.new
+    @word = Word.new('hello')
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,14 @@
 require 'game'
 
 RSpec.describe Game do
-  it "it will instantiate an instance of Display and return the intro message" do
+  it "will instantiate an instance of Display and return the intro message" do
     game = Game.new
     expect(game.display.intro).to eq("Welcome to Hangman")
+  end
+
+  it "will instantiate an instance of Word and return false when calling the solved? method" do
+    game = Game.new
+    expect(game.word.solved?).to eq(false)
   end
 
 end
