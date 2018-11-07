46c5749526b5fba4efaa312a1299c4dd2e0d463a
Elle H <itsellej@gmail.com>
2018-11-07 16:03:26 +0000

    Refactor initialize method

--- lib/game.rb
+++ lib/game.rb
@@ -6,9 +6,9 @@ class Game
 
   attr_reader :display, :word, :lives
 
-  def initialize
-    @display = Display.new
-    @word = Word.new('hello')
-    @lives = Lives.new
+  def initialize(display=Display.new, word=Word.new('hello'), lives=Lives.new)
+    @display = display
+    @word = word
+    @lives = lives
   end
 end
