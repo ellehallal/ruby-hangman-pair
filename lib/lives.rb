938433accaac94e9ed21ed8403c6caa959ac5d03
Elle H <itsellej@gmail.com>
2018-11-07 14:01:09 +0000

    Create subtract_life method. Passing test: removes one life

--- lib/lives.rb
+++ lib/lives.rb
@@ -1,7 +1,11 @@
 class Lives
   attr_reader :lives
-  
+
   def initialize
     @lives = 8
+  end
+
+  def subtract_life
+    @lives -= 1
   end
 end
--- spec/lives_spec.rb
+++ spec/lives_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Lives do
     lives = Lives.new
     expect(lives.lives).to eq(8)
   end
+
+  it "removes one life" do
+    lives = Lives.new
+    expect(lives.subtract_life).to eq(7)
+  end
+
+
+
+
 end
